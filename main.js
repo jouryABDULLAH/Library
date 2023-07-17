3b3a3e729189ccdd7c38340d2d435e2d0090e48e
JouryABDULLAH <jours-222@outlook.com>
2023-07-17 16:34:01 -0700

    set up js

---
+++ main.js
@@ -0,0 +1,6 @@
+const tempQoute = document.getElementById('temp-qoute');
+const addButton = document.getElementById('add-book')
+
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+})
